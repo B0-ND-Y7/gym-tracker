4d56152c86680babdcffde555a2204b444f21ca2
B0-ND-Y7 <harryscottbond@gmail.com>
2026-08-30 13:49:52 +0100

    Gym tracker v14.0.3 - deploy fix

--- gym-tracker-v14.0.3/sw.js
+++
@@ -1,4 +0,0 @@
-// v14 intentionally does not register a service worker.
-// Existing registrations are removed by index.html during the v14.0.3 boot.
-self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
