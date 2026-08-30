8022fe05aff67773481e8f5e7046ee9359c5709a
B0-ND-Y7 <harryscottbond@gmail.com>
2026-08-30 15:02:50 +0100

    Gym tracker v15.0 - muscle based workout rebuild

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-// v14 intentionally does not register a service worker.
-// Existing registrations are removed by index.html during the v14.0.3 boot.
-self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+const CACHE='gym-tracker-v15.0';
+const CORE=['./','./index.html','./manifest.webmanifest','./exercise-library.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{if(e.request.url.startsWith(self.location.origin)){const copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy))}return r}).catch(()=>caches.match('./index.html'))))});
